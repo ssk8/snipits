e69cdaf1a33f3824bc11c89e4965977511f0c243
ssk8 <ssk8@users.noreply.github.com>
2022-06-26 14:34:45 -0500

    Update renamer.py

--- renamer.py
+++ renamer.py
@@ -5,6 +5,7 @@ print('--------rename-------')
 basepath = Path(__file__).parent.absolute()
 
 for n, f in enumerate(sorted(basepath.glob('*.mkv'))):
-    new_name = basepath / f"S02E{n+13} {f.name}"
+    name = f.name.replace("_", " ")[7:]
+    new_name = basepath / f"{name}"
     print(new_name)
     #f.rename(new_name)

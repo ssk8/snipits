5f6f6d6cdc5c023e47dd74df18e000208311c068
ssk8 <ssk8@users.noreply.github.com>
2022-10-04 21:57:28 -0500

    Update remote_renamer.py

--- remote_renamer.py
+++ remote_renamer.py
@@ -1,25 +1,45 @@
-from pathlib import Path
-import paramiko
+#!/usr/bin/python3
 
-key_path = "/home/curt/.ssh/known_hosts"
+from pathlib import Path
+from os import environ
+import paramiko
+import re
+from typing import List
+
 kodi_address = "192.168.1.110"
+#kodi_hd_path = Path("/media/sda1-usb-WDC_WD20_EZRZ-00")
 kodi_hd_path = Path("/media/sda1-ata-WDC_WD20EZRZ-00Z")
-ori_path = kodi_hd_path / 'TV Shows' / 'oldname'
-target_path =  kodi_hd_path / 'TV Shows' / 'newname'
+ori_path = kodi_hd_path / 'TV Shows' / 'old name'
+target_path =  kodi_hd_path / 'TV Shows' / 'new name'
+
+def get_remote(sftp_client: paramiko.sftp_client.SFTPClient, path, dirs=True) -> List[str]:
+    """returns a list of remote directories at 'path' on 'sftp_client' if 'dirs' or list of files if not 'dirs'"""
+    mode = 16877*dirs or 33188
+    return [f.filename for f in sftp_client.listdir_attr(str(path)) if f.st_mode==mode]
+
 
 def get_sftp_client(address: str) -> paramiko.sftp_client.SFTPClient:
     ssh_client=paramiko.SSHClient()
-    ssh_client.load_host_keys(key_path)
+    ssh_client.load_host_keys("/home/curt/.ssh/known_hosts")
     ssh_client.connect(hostname=address, username='root')
     return ssh_client.open_sftp()
 
 
 def main():
     kodi_sftp = get_sftp_client(kodi_address)
-    ori_files = kodi_sftp.listdir_attr(f"{ori_path}")
+    ori_files = get_remote(kodi_sftp, ori_path, dirs=False)
+
+    fixmatch = re.compile('S\d{2}E\d{2}')
     for f in ori_files:
-        print(f"{f.filename} is a {(f.st_mode==16877)*'directory'}{(f.st_mode==33188)*'file'}")
-        #kodi_sftp.rename(str(ori_path/f), str(target_path/new_name))
+        if se:=fixmatch.search(f):
+            s, e = int(se.group()[1:3]), int(se.group()[4:])
+            if s==1 and e>12:
+                s, e = divmod(e, 12)
+                s+=1
+                new_name = fixmatch.sub(f'S{s:02}E{e:02}', f)
+                kodi_sftp.rename(str(ori_path/f), str(target_path/new_name))
+    if ori_path != target_path:
+        kodi_sftp.rmdir(str(ori_path))
 
 if __name__ == "__main__":
     main()

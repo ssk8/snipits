998fc9926b52ed0038268f1a50a02d8e7d4489b0
ssk8 <ssk8@users.noreply.github.com>
2024-07-01 15:06:22 +0000

    Update quick_web_temp.py

--- quick_web_temp.py
+++ quick_web_temp.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return get_temp()
+    return f"Temp is {get_temp():.1f}°C"
+
+@app.route('/json')
+def json():
+    return {'temp_c':get_temp(),'unit_c':'°C'}
+
 
 def get_temp():
     probe_adress = "/sys/bus/w1/devices/28-0000067040a8/w1_slave"
@@ -14,7 +19,8 @@ def get_temp():
             raw_reading = file.read()
 
             temp = int(raw_reading[~5:])/1000
-            return f"Temp is {temp:.2f}°C"
+            return temp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port="8080", debug=True) 
+

b52f1f7355d5f054d26459c2b09be4c102aa560f
ssk8 <ssk8@users.noreply.github.com>
2024-11-16 04:43:54 +0000

    Update subs_rename.py

--- subs_rename.py
+++ subs_rename.py
@@ -1,13 +1,9 @@
 from pathlib import Path
 from shutil import copy
 
-#basepath = Path(__file__).parent.absolute()
-basepath = Path("/home/curt/Downloads/Subs/")
-new_path = basepath.parent/"Subs_renamed"
-new_path.mkdir()
+basepath = Path(__file__).parent.absolute()/"Subs"
 
 for d in [f for f in basepath.glob("*") if f.is_dir()]:
     for sub in [f for f in d.glob("*English.srt")]:
         new_name = f"{d.name}_{sub.name}"
-        copy (sub, new_path/new_name)
-        print(new_name)
+        copy (sub, new_name)
